fa3f944a8566fe9474574b209274a57cb864152a
srinman <smanivel@microsoft.com>
2023-07-01 01:24:40 -0400

    updated code

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     #return render_template('index.html')
-    response = {'message': 'Hello World'}
+    response = {'message': 'Hello World Test'}
     return jsonify(response)
 #      curl -X GET 'localhost:5000/'
 
@@ -38,6 +38,11 @@ def get_values():
     return json.dumps(values_list)
 #      curl -X GET 'localhost:5000/values'
 
+
+#Implement GET method for all keys and values
+@app.route('/all', methods=['GET'])
+def get_all():
+    return jsonify(datadict)
 
 #Implement DELETE method for a specific route - deleting a key
 @app.route('/keys/<key>', methods=['DELETE'])
@@ -72,6 +77,38 @@ def add_data():
     datadict[key] = value
     return jsonify({'message': f'{key} added/updated with value {value}'})
 #      curl -X POST 'localhost:5000/keys' -H "Content-Type: application/json"  --data '{"key": "key2","value":"val2"}'
+
+#Implement POST method to initialize the dictionary with example keys and values with 10 sets
+# curl -X POST 'localhost:5000/init' -H "Content-Type: application/json"  --data '{"count": 10}'
+# curl -X POST 'https://srinmanapitest.azurewebsites.net/init' -H "Content-Type: application/json"  --data '{"count": 10}'
+@app.route('/init', methods=['POST'])
+def init_data():
+    message = request.get_json()
+    print(message)
+    count = message['count']
+    for i in range(count):
+        key = 'key' + str(i)
+        value = 'val' + str(i)
+        datadict[key] = value
+    return jsonify({'message': f'{count} keys added/updated'}) 
+
+
+#Implement POST method to read values from Redis database and populate the dictionary 
+# curl -X POST 'localhost:5000/readredis' -H "Content-Type: application/json"  --data '{"count": 10}'
+# curl -X POST 'https://srinmanapitest.azurewebsites.net/readredis' -H "Content-Type: application/json"  --data '{"count": 10}'
+#@app.route('/readredis', methods=['POST'])
+#def read_redis():
+    # Connect to Redis database
+#    r = redis.Redis(host='localhost', port=6379, db=0)
+    # Read all keys from Redis database
+#    keys = r.keys()
+    # Read all values from Redis database
+#    values = r.mget(keys)
+    # Populate the dictionary with keys and values
+#    for i in range(len(keys)):
+#        datadict[keys[i]] = values[i]
+#    return jsonify({'message': f'{len(keys)} keys added/updated'})
+
 
 
 
